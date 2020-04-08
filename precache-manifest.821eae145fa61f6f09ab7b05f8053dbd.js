2310f8e7a69731fa2f377316cb2dcb028dc68d7b
gustavo-tp <gustavo1190.gt@gmail.com>
2020-04-08 16:38:52 -0300

    Updates

---
+++ precache-manifest.821eae145fa61f6f09ab7b05f8053dbd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed7801d3a83f79e339209bf5776e850",
+    "url": "/tindev/index.html"
+  },
+  {
+    "revision": "5db09216f0d66d110877",
+    "url": "/tindev/static/css/main.86c31a7d.chunk.css"
+  },
+  {
+    "revision": "228843e98dfdac7df5b8",
+    "url": "/tindev/static/js/2.91b41970.chunk.js"
+  },
+  {
+    "revision": "5db09216f0d66d110877",
+    "url": "/tindev/static/js/main.afd49b75.chunk.js"
+  },
+  {
+    "revision": "b954a5359cbabd7d0c63",
+    "url": "/tindev/static/js/runtime-main.2799e1df.js"
+  },
+  {
+    "revision": "d4ec34e81346484c83328c449a35141b",
+    "url": "/tindev/static/media/dislike.d4ec34e8.svg"
+  },
+  {
+    "revision": "21c10b2613fbc1b8b36859e433819ed1",
+    "url": "/tindev/static/media/itsamatch.21c10b26.png"
+  },
+  {
+    "revision": "542059a425f4220819edb0e56c735bb7",
+    "url": "/tindev/static/media/like.542059a4.svg"
+  },
+  {
+    "revision": "6a01ceef657ab22cc670a0a1ae108176",
+    "url": "/tindev/static/media/logo.6a01ceef.svg"
+  }
+]);
